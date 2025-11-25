2e90eb19fa2dfa3096e3ce7e8ac1f6ddd8cebacf
Luiz Felipe de Jesus <luizfelipekoyama@outlook.com>
2025-11-25 00:59:25 +0000

    adiciona interface e componente para feedback detalhado da previsão de depressão

--- student-depression-app/src/app/repository/student-depression-repository.tsx
+++ student-depression-app/src/app/repository/student-depression-repository.tsx
@@ -1,5 +1,6 @@
 import mapQuestions, { RawQuestion } from "@/app/mappers/question-maper"
 import { QuestionBase } from "../model/question-base"
+import { SurveyPredictDepressionResponse } from '../model/survey_predict_depression_response';
 
 interface StudentDepressionRepository {
     getQuestions(): Promise<QuestionBase[]>
@@ -20,5 +21,26 @@ export class StudentDepressionRepositoryImp implements StudentDepressionReposito
         }
 
         return []
+    }
+
+    static async requestFeedback(responses: Record<string, string | number>): Promise<SurveyPredictDepressionResponse> {
+        try {
+            const response = await fetch(`${BASE_URL}/predict`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(responses),
+            });
+
+            if (response.ok) {
+                const data: SurveyPredictDepressionResponse = await response.json();
+                return data
+            }
+
+            throw new Error(`Error: ${response.status} ${response.statusText}`);
+        } catch (error) {
+            throw error;
+        }
     }
 }
