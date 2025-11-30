be082f24cc57046803bfb7617ff79f5f22e7c6e3
Luiz Felipe de Jesus <luizfelipekoyama@outlook.com>
2025-11-30 22:39:23 +0000

    permissão pro frontend acessar o backend

--- student-depression-api/main.py
+++ student-depression-api/main.py
@@ -10,6 +10,7 @@ from src.model.questions import router as questions_router
 
 origins = [
         "http://localhost:3000"
+        "https://projengsoftware-o465.onrender.com/"
     ]
 
 app = FastAPI()
